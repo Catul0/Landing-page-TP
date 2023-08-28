a73845455e6a6f318b7a3aff5b2f73854775b703
catul0 <valesigaudo1@gmail.com>
2023-08-28 10:15:46 -0300

    arreglo de rutas de imagenes

--- src/components/Planes.tsx
+++ src/components/Planes.tsx
@@ -1,6 +1,8 @@
 
 import TarjetaPlanes from './TarjetaPlanes'
-
+import freePlan from '../../public/free-plan.svg';
+import standardPlan from '../../public/standard-plan.svg';
+import premiumPlan from '../../public/premium-plan.svg'
 export const Planes = () => {
     return (
         <div className=' lg:px-24 2xl:px-[350px] bg-blue-300 py-8 mt-16 ' id='precios'>
@@ -14,17 +16,17 @@ export const Planes = () => {
             </div>
             <div className=' hidden lg:flex  mt-8 place-content-around'>
 
-                <TarjetaPlanes imagen='../../public/free-plan.png' nombree='Free Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' />
-                <TarjetaPlanes imagen='../../public/standard-plan.png' nombree='Standard Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' precio={9} />
-                <TarjetaPlanes imagen='../../public/premium-plan.png' nombree='Premium Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' item6='Get New Features' precio={12} />
+                <TarjetaPlanes imagen={freePlan} nombree='Free Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' />
+                <TarjetaPlanes imagen={standardPlan} nombree='Standard Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' precio={9} />
+                <TarjetaPlanes imagen={premiumPlan} nombree='Premium Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' item6='Get New Features' precio={12} />
 
             </div>
             <div className="flex flex-row lg:hidden  mt-8 ">
                 <div className="basis-1/10 "></div>
                 <div className="basis-8/10 place-content-around">
-                    <TarjetaPlanes imagen='../../public/free-plan.png' nombree='Free Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' />
-                    <TarjetaPlanes imagen='../../public/standard-plan.png' nombree='Standard Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' precio={9} />
-                    <TarjetaPlanes imagen='../../public/premium-plan.png' nombree='Premium Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' item6='Get New Features' precio={12} />
+                    <TarjetaPlanes imagen={freePlan} nombree='Free Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' />
+                    <TarjetaPlanes imagen={standardPlan} nombree='Standard Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' precio={9} />
+                    <TarjetaPlanes imagen={premiumPlan} nombree='Premium Plan' item1='Unlimited Bandwitch' item2='Encrypted Connection' item3='Yes Traffic Logs' item4='Works on All Divices' item5='Connect Anywhere' item6='Get New Features' precio={12} />
                 </div>
                 <div className="basis-1/10 "></div>
             </div>
